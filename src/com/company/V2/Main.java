aa019cc53f4c709a39079994fb59d67327f0414e
ishini <amarasinghaishini@gmail.com>
2023-11-23 22:49:06 +0530

    Read numbers from text file & do operation

--- src/com/company/V2/Main.java
+++ src/com/company/V2/Main.java
@@ -1,10 +1,16 @@
 package com.company.V2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //Check whether the argument is not null or null
-        if (args.length == 0){
+        if (args.length == 0) {
             System.out.println("Please provide the operation as an argument");
             return;
         }
@@ -17,6 +23,24 @@ public class Main {
             return;
         }
 
-        System.out.println(args[0]);
+        //read the numbers from text file
+        List<String> numbersStr = Files.readAllLines(
+                Paths.get("C:/Users/ishini dimani/Desktop/Calculator 2/Numbers.txt")
+        );
+
+        double number1 = Double.parseDouble(numbersStr.get(0));
+        double number2 = Double.parseDouble(numbersStr.get(1));
+
+        double result = 0;
+        if (operator.equals("Add")) {
+            result = number1 + number2;
+        } else if (operator.equals("Sub")) {
+            result = number1 - number2;
+        } else if (operator.equals("Mul")) {
+            result = number1 * number2;
+        }
+
+
+        System.out.println("The result is " + result);
     }
 }
